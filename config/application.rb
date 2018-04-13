60bb388451a31e6d22e6bda7fe7eeba50c651701
BootCoder <bootcoder@gmail.com>
2018-04-12 18:04:08 -0700

    resolve upgrade bug in app.rb

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module TakeHomeChallenge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Ensure conversion of old sqlite3 bool to ints
+    config.active_record.sqlite3.represent_boolean_as_integer = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
