ada567438a6ca90efe178a78352f233ba599d9ab
BootCoder <bootcoder@gmail.com>
2018-04-13 17:42:57 -0700

    wip ajax for index form

--- config/initializers/new_framework_defaults_5_1.rb
+++ config/initializers/new_framework_defaults_5_1.rb
@@ -7,7 +7,7 @@
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
 # Make `form_with` generate non-remote forms.
-Rails.application.config.action_view.form_with_generates_remote_forms = false
+Rails.application.config.action_view.form_with_generates_remote_forms = true
 
 # Enable per-form CSRF tokens. Previous versions had false.
 Rails.application.config.action_controller.per_form_csrf_tokens = false
